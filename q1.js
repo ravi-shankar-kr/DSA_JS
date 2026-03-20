5df912ebf88bb61e242e4066aa1863505436770f
Ravi_Shankar_kr <ravi896959@gmail.com>
2026-03-21 02:03:25 +0530

    29 Q done

--- q1.js
+++
@@ -1,8 +0,0 @@
-// Q1 – Sum of two integers
-// Difficulty: Easy | Topic: BASICS
-
-let a = 10;
-let b = 20;
-
-let c = a+b
-console.log(c);
--- q3.js
+++
@@ -1,8 +0,0 @@
-// Q3 – Accept two integers from user and print the sum (Ex - The sum of 45 & 12 = 57)
-// Difficulty: Easy | Topic: BASICS
-
-let a = Number(prompt("Enter first Num: "));
-let b = Number(prompt("Enter second Num: "));
-
-let sum = a+b
-console.log(`The sum of ${a} & ${b} = ${sum}`);
